5045977254e96e64de5d2b4cb6adc0e02d79456e
rokydas <droky136@gmail.com>
2021-08-19 02:19:21 +0600

    register done with pass hashing

--- routes/auth.js
+++ routes/auth.js
@@ -1,37 +1,43 @@
 const router = require('express').Router()
 const User = require('../model/User')
-
-// validation
-const Joi = require('@hapi/joi')
-const schema = {
-    name: Joi.string().min(3).required(),
-    email: Joi.string().min(5).required(),
-    password: Joi.string().min(3).required()
-}
+const bcrypt = require('bcryptjs')
+const { registerValidation } = require('../validation')
 
 router.post('/register', async (req, res) => {
 
     // validation before making user
-    const {error} = Joi.validate(req.body, schema);
-    if(error == null) {
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        })
-        try {
-            const savedUser = await user.save()
-            res.send(savedUser)
-        } catch (err) {
-            res.status(404).send({err: err})
-        }
-    }
-    else {
-        res.status(400).send({err: error.details[0].message})
-    }
-    
+    const error = registerValidation(req.body)
+    if (error) return res.status(400).send({ err: error.details[0].message })
 
-    
+    // checking if the user's email exists in the db
+    const emailExist = await User.findOne({ email: req.body.email })
+    if (emailExist) return res.status(409).send({ err: "user is already registered" })
+
+    // hashing password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    console.log(hashPassword)
+
+    // adding user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    })
+    try {
+        const savedUser = await user.save()
+        res.send(savedUser)
+    } catch (err) {
+        res.status(404).send({ err: err })
+    }
+
+
+
+
+
+
+
+
 })
 
 // router.post('/login')
---
+++ validation.js
@@ -0,0 +1,15 @@
+// validation
+const Joi = require('@hapi/joi')
+
+// registerValidation 
+const registerValidation = (body) => {
+    const schema = {
+        name: Joi.string().min(3).required(),
+        email: Joi.string().min(5).required(),
+        password: Joi.string().min(3).required()
+    }
+    const {error} = Joi.validate(body, schema);
+    return error
+} 
+
+module.exports.registerValidation = registerValidation
