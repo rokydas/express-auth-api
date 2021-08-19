372f02003a44b54e1c9c4b12225f7afd11f1cc9a
rokydas <droky136@gmail.com>
2021-08-19 17:26:26 +0600

    script updated

--- index.js
+++ index.js
@@ -17,4 +17,6 @@ app.use(express.json());
 app.use('/api/user', authRoute)
 
 
-app.listen(process.env.PORT || 5000); 
+app.listen(process.env.PORT || 5000, () => {
+    console.log("server is running")
+}); 
